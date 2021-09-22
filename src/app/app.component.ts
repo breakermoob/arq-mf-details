34d6e8c73221a2e8bf879f79e63be3057eeb90e1
lion <breakermoob@hotmail.com>
2021-09-22 03:09:01 -0500

    First commit - base Angular details config with single spa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mf-details',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
